8199024a7b94d54dc61aef6265f39fe506bc5b63
credelsamck <credelsamck@gmail.com>
2026-06-04 22:26:02 +0100

    modif

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
-$host = "localhost";
-$dbname = "boutique";
-$username = "root";
-$password = "";
+$host     = getenv('MYSQLHOST');
+$dbname   = getenv('MYSQLDATABASE');
+$username = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$port     = getenv('MYSQLPORT');
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
